2f81ce0148522b2f5e509ac352f001344f363fd4
Martin Levesque <levesque.martin@gmail.com>
2017-10-07 22:14:51 -0400

    progress

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const watch = require("watch");
 const fs = require("fs");
 const path = require("path");
 const getFSize = require("get-folder-size");
+const rmDir = require("rimraf");
+const dirTree = require("directory-tree");
 
 class LimitDirs {
-  constructor(rootDir, subDirs = [], options = {}) {
-    this.rootDir = rootDir;
-    this.subDirs = subDirs;
-    this.autoDiscoverNewSubDirs = options.autoDiscoverNewSubDirs;
+  constructor(options = {}) {
+    this.rootDir = options.rootDir || "./";
+    this.subDirs = options.subDirs || [];
+    this.level = options.level || 1;
+    this.autoDiscoverNewSubDirs = options.autoDiscoverNewSubDirs || false;
+    this.defaultLimitMB = options.defaultLimitMB || 1000000;
     this.activatedWatches = {};
-    this.defaultLimitMB = options.defaultLimitMB;
+    this.verbose = options.verbose || false;
 
-    for (let d of subDirs) {
+    for (let d of this.subDirs) {
       this.activateWatch(d.subdir, d.limitMB);
     }
 
@@ -23,46 +27,164 @@ class LimitDirs {
 
   }
 
+  _log(msg) {
+    if (this.verbose) {
+      console.log((new Date()) + ": " + msg);
+    }
+  }
+
+  _getFoldersWithLevel(dirs, level, curLevel, result = []) {
+    console.log("----------------====================");
+    console.log(dirs);
+    console.log("cur level = " + curLevel);
+    console.log("result = ");
+    console.log(result);
+
+    for (let f of dirs) {
+      if (f.type == "directory") {
+        if (level == curLevel) {
+          console.log("adding " + f.path);
+          result.push(f);
+        }
+        else if (curLevel < level) {
+          result = this._getFoldersWithLevel(f.children, level, curLevel + 1, [...result]);
+        }
+      }
+    }
+
+    return result;
+  }
+
   scanRoot() {
+
+    let finalR = this._getFoldersWithLevel(dirTree(this.rootDir).children, this.level, 1, []);
+
+    console.log("finall");
+    console.log(finalR);
+
     fs.readdir(this.rootDir, (err, dirs) => {
       if ( ! err) {
         for (let dir of dirs) {
-          this.activateWatch(dir, this.defaultLimitMB);
+          this.activateWatch(this.rootDir + "/" + dir, this.defaultLimitMB);
         }
       }
     });
   }
 
-  pretty() {
-    console.log("root dir ");
-    console.log(this.rootDir);
-    console.log("active watches ");
-    console.log(this.activatedWatches);
+  _deleteFile(f) {
+    setTimeout(() => {
+      fs.unlink(f, (err) => {
+        if ( ! err) {
+          this._log("deleted " + f);
+        }
+      });
+    }, 3000);
   }
 
-  _initWatch(dir, initialMB) {
+  _isSameAction(previousActionInfo, curAction, f) {
+    return previousActionInfo.action == curAction &&
+      previousActionInfo.f == f && (new Date() - previousActionInfo.ts) <= 2000;
+  }
 
-    console.log("dir = " + dir);
-    console.log(initialMB);
+  _updPreviousAction(previousAction, action, file) {
+    previousAction.action = action;
+    previousAction.f = file
+    previousAction.ts = new Date();
+  }
 
-    watch.watchTree(dir, function (f, curr, prev) {
+  _sizeFileOrFolder(f, stat) {
+    return new Promise((resolve, reject) => {
+      if (stat.isDirectory()) {
+        getFSize(f, (err, size) => {
+          if ( ! err) {
+            resolve({
+              "size": size,
+              "type": "folder"
+            });
+          } else {
+            reject(err);
+          }
+        });
+      } else {
+        resolve({
+          "size": stat.size,
+          "type": "file"
+        });
+      }
+    });
+  }
+
+  _initWatch(dir, currentMB, limitMB) {
+    this._log("starting watch of " + dir + " starting size " + currentMB + " MB of " + limitMB);
+
+    const previousAction = {
+      "action": "",
+      "f": "",
+      "ts": new Date()
+    };
+
+    let timeoutCheckSize = null;
+
+    watch.watchTree(dir, (f, curr, prev) => {
+        this._log("watch treee of ");
+        this._log(f);
         if (typeof f == "object" && prev === null && curr === null) {
           // Finished walking the tree
-          console.log("done walking the tree...");
-        } else if (prev === null) {
+
+        } else if (prev === null && ! this._isSameAction(previousAction, "C", f)) { // CREATED
           // f is a new file
-          console.log("new f");
-        } else if (curr.nlink === 0) {
-          // f was removed
-          console.log("removed ..");
-        } else {
-          // f was changed
-          console.log("changed ..");
-          console.log("cur = ");
-          console.log(curr);
-          console.log("prev = ");
-          console.log(prev);
+
+          this._sizeFileOrFolder(f, curr).then((sizeInfo) => {
+            currentMB += sizeInfo.size / 1000 / 1000;
+            this._log("[CREATED] " + f + " new size of " + dir + " = " + currentMB + " MB");
+
+            if (currentMB > limitMB) {
+              rmDir(f, (err) => {
+                this._log(err);
+              });
+            }
+
+          }).catch((err) => {
+            console.log("errr sizeee");
+            this._log(err);
+          });
+
+          this._updPreviousAction(previousAction, "C", f);
+        } else if (curr.nlink === 0 && ! this._isSameAction(previousAction, "R", f)) { // REMOVED
+          if (prev && prev.size) {
+            currentMB -= prev.size / 1000 / 1000;
+            this._log("[REMOVED] " + f + " new size of " + dir + " = " + currentMB + " MB");
+          }
+
+          this._updPreviousAction(previousAction, "R", f);
+        } else if (curr && prev && ! this._isSameAction(previousAction, "M", f)) { // CHANGED
+          let changedBy = curr.size - prev.size;
+
+          currentMB += changedBy / 1000 / 1000;
+          this._log("[CHANGED] " + f + " new size of " + dir + " = " + currentMB + " MB");
+
+          if (currentMB > limitMB) {
+              rmDir(f, (err) => {
+                this._log(err);
+              });
+          }
+
+          this._updPreviousAction(previousAction, "M", f);
         }
+
+        if ( ! timeoutCheckSize) {
+          timeoutCheckSize = setTimeout(() => {
+            getFSize(dir, (err, size) => {
+              if ( ! err) {
+                currentMB = size / 1000 / 1000;
+                this._log("Refreshed new size of " + dir + " = " + currentMB + " MB");
+              }
+
+              timeoutCheckSize = null;
+            });
+          }, 3000);
+        }
+
       })
   }
 
@@ -78,21 +200,30 @@ class LimitDirs {
               let sizeMB = size / 1000 / 1000;
 
               if ( ! this.activatedWatches[normDir]) {
-                this._initWatch(normDir, sizeMB);
+                this._initWatch(normDir, sizeMB, limitMB);
                 this.activatedWatches[normDir] = {
                   "status": "active",
                   limitMB
                 };
               }
+            } else {
+              this._log("issue " + err);
             }
           });
         }
+      } else {
+        this._log("issue " + err);
       }
     });
   }
 }
 
-let d = new LimitDirs("./", [/* {"subdir": '...', limitMB: 1000} */],
-  { "autoDiscoverNewSubDirs": true, "defaultLimitMB": 1000 });
-
-d.pretty();
+new LimitDirs(
+  {
+    "rootDir": "/home/martin/dummy/",
+    "level": 2,
+    "subDirs": [],
+    "autoDiscoverNewSubDirs": true,
+    "defaultLimitMB": 100,
+    "verbose": true
+  });
